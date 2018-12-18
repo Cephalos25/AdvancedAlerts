914bc23d310e0df2a44363c151abb5d9a931d8be
Cephalos <tscephalos@gmail.com>
2018-12-17 22:50:53 -0800

    Splitting commits in an attempt to make them work.

--- app/src/main/java/com/example/advancedalarm/putNotificationReceiver.java
+++ app/src/main/java/com/example/advancedalarm/putNotificationReceiver.java
@@ -4,10 +4,15 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
-public class putNotificationReceiver extends BroadcastReceiver {
+public class PutNotificationReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-
+        byte[] bytes = intent.getByteArrayExtra("alarm");
+        int notificationID = intent.getIntExtra("notification ID", 1023);
+        Intent serviceIntent = new Intent(context, PutNotificationService.class);
+        serviceIntent.putExtra("alarm", bytes);
+        serviceIntent.putExtra("notification ID", notificationID);
+        context.startService(serviceIntent);
     }
 }
